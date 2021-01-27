a2074398f88cbde3222ba523067f0c7ec1976585
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-01-27 01:12:16 -0800

    m

--- server.js
+++ server.js
@@ -28,21 +28,21 @@ const newNoteList = [];
 
 
 //// start here in the public folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //// DEFAULT TO HOME by startign at the directory and ending at the index.html  (navigational purposes)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 //// Go to notes by startind at the directory and ending at the notes.html (navigational purposes)
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // get method establishing that the api/notes is linked to the db.json file for future reading and writing. if something is wrong send an error, then parse the reponse from the data pulled from the db file
 app.get("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
   });
@@ -50,7 +50,7 @@ app.get("/api/notes", (req, res) => {
 
 // post method establishing that the api/notes is linked to the db.json file for future reading and writing. if something is wrong send and error. parse the reponse from the data pulled from the db file
 app.post("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
     if (err) throw err;
     const dataBase = JSON.parse(data); 
     dataBase.push(req.body);
@@ -63,40 +63,15 @@ app.post("/api/notes", (req, res) => {
       };
       newNoteList.push(newNote);
     }
-    fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newNoteList, null, 2), (err) => {
+    fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newNoteList, null, 2), (err) => {
       if (err) throw err;
       res.json(req.body);
     });
   });
-  console.log("not posted");
+  console.log('posted');
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    //var id = parseInt(req.params.id);
-    //const dataBase = JSON.parse(data); 
-  //   fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
-  //   if (err) throw err;
-    
-    
-  //   const dataBase = JSON.parse(data);   
-  //   for (let i = 0; i < dataBase.length; i++) {
-  //     if (i !== id) {
-  //       const newNote = {
-  //         title: dataBase[i].title,
-  //         text: dataBase[i].text,
-  //         id: newNoteList.length
-  //       };
-
-  //       newNoteList.push(newNote);
-  //     }
-  //   }
-
-  //   fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(newNoteList, null, 2), (err) => {
-  //     if (err) throw err;
-  //     res.json(req.body);
-  //   });
-  // });
-
   console.log('we want to delete this note, and we are working on it');
 });
 
