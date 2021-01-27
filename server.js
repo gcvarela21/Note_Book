3e28b607ce3e925a4835f6eefb7da7f74334e06b
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-01-26 21:05:55 -0800

    set up the get method to navigate to the index html with the *

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 3000;
 // Lets setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
 
 // Setup listener and error catch
 app.listen(PORT, function(error) {
@@ -19,7 +18,30 @@ app.listen(PORT, function(error) {
     } else {
         console.log("app/express is listening on PORT: " + PORT);
     }
-});  
+}); 
+///////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+// My Global Empty Array 
+const newNoteList = [];
+const dataBase = JSON.parse(data);
+const id = parseInt(req.params.id);
+
+
+//// start here in the public folder
+app.use(express.static('public'));
+
+//// DEFAULT TO HOME by startign at the directory and ending at the index.html 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
+
+
+
+
 
 
 
